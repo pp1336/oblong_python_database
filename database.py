fc4bbf81ae7538593578a25c77475c91b506d2d4
pp1336 <pp1336@users.noreply.github.com>
2016-11-05 17:05:01 +0000

    crate_table

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 import pg
 
-class Database_connector:
+class Database_manager:
 
-    _database
-    isOpen
+    _create_table = "CREATE TABLE table_name( "
+    _primary_key = "PRIMARY KEY( "
 
     def __init__(self, dbname, host, user):
         self._database = pg.connect(dbname=dbname, host=host, user=user)
@@ -18,6 +18,29 @@ class Database_connector:
 
     def _update_(self, query):
         self._database.query(query)
+
+    def create_table(self, cols, primary):
+        cmd = Database_manager._create_table
+        l1 = len(cols)
+        l2 = len(primary)
+        count = 0
+        for (col, t) in cols:
+            if (l2 > 0 or count < l1 - 1):
+                cmd = "%s %s %s, " % (cmd, col, t)
+                count += 1
+            else:
+                cmd = "%s %s %s" % (cmd, col, t)
+        if (l2 > 0):
+            cmd = "%s %s" % (cmd, _primary_key)
+            count = 0
+            for k in primary:
+                if (count < l2 - 1):
+                    cmd = "%s %s, " % (cmd, k)
+                    count += 1
+                else:
+                    cmd = "%s %s )" % (cmd, k)
+        cmd = "%s );" % (cmd)
+        self._update_(cmd)
 
 
     
