9d0a8a512d2249aa894fb6ad88d46f23e67bf9eb
pp1336 <pp1336@users.noreply.github.com>
2016-11-05 16:25:13 +0000

    initial

--- database.py
+++ database.py
@@ -3,12 +3,21 @@ import pg
 class Database_connector:
 
     _database
+    isOpen
 
     def __init__(self, dbname, host, user):
-        _database = pg.connect(dbname=dbname, host=host, user=user)
+        self._database = pg.connect(dbname=dbname, host=host, user=user)
+        self.isOpen = True
 
-    def _query_(query):
-        return (_database.query(query)).dictresult()
+    def close(self, query):
+        self._database.close()
+        self.isOpen = False
+
+    def _query_(self, query):
+        return (self._database.query(query)).dictresult()
+
+    def _update_(self, query):
+        self._database.query(query)
 
 
     
